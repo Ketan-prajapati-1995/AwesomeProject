93e53d44843f3a4a74d2076c2af471531413eaf5
Ketan-prajapati-1995 <ketanprajapati1789@gmail.com>
2024-06-04 12:54:24 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -3,7 +3,7 @@ import {Text,View} from 'react-native'
 const kk = () => {
 return (
   <View style = {{backgroundColor:'yellow'}}>
-    <Text style={{color:"blue", backgroundColor:"red",textAlign:"center", justifyContent:'center',margin:80,fontSize:25}}>My name is ketan</Text>
+    <Text style={{color:"blue", backgroundColor:"red",textAlign:"center", justifyContent:'center',margin:80,fontSize:50}}> hello My name is ketan</Text>
   
     
      </View>
