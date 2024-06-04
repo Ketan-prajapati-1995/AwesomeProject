090d2eb44428c843cf0b1f53390aca9c8c9b839c
Ketan-prajapati-1995 <ketanprajapati1789@gmail.com>
2024-06-04 12:48:27 +0530

    Initial commit

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Text,View} from 'react-native'
+const kk = () => {
+return (
+  <View style = {{backgroundColor:'yellow'}}>
+    <Text style={{color:"blue", backgroundColor:"red",textAlign:"center", justifyContent:'center',margin:80,fontSize:25}}>My name is ketan</Text>
+  
+    
+     </View>
+
+)
+
+}
+
+export default kk
